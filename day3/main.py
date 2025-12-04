01ca1c727987aaeb0bf20ad56bfd8c6b5ea618ad
vinceausrion <vincent.llauderes@asurion.com>
2025-12-04 09:23:36 +0800

    day 2

--- day3/main.py
+++ day3/main.py
@@ -3,7 +3,7 @@
 # Batteries are arranged into banks; each line of digits in your input
 # Exactly two batteries
 
-def joltage():
+def joltage_1():
   with open("input.txt", "r") as f:
     batteries = f.read().split('\n')
     jol = []
@@ -18,5 +18,25 @@ def joltage():
       jol.append(max(hash, key=hash.get))
     print(sum(int(val) for val in jol))
 
+# Part 2
+# Exactly 12 batteries
+def joltage_2():
+  with open("input.txt", "r") as f:
+    batteries = f.read().split('\n')
+    jol = []
+    for i in range(0, len(batteries)):
+      items = list(batteries[i])
+      n = len(items)
+      k = 12
+      stack = []
+      removals_left = n - k
+      for digits in items:
+        while stack and removals_left > 0 and stack[-1] < digits:
+          stack.pop()
+          removals_left -= 1
+        stack.append(digits)
+      jol.append(int(''.join(stack[:12])))
+    print(sum(jol))
+
 if __name__ == "__main__":
-  joltage()
+  joltage_2()
